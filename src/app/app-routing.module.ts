a0d205ee9e51435b03e276c5675cd17fcd9ab1a6
unknown <Eloy>
2023-09-03 20:55:18 -0600

    se agrega menu se inicia dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent}
+  {
+    path:'',
+    component:PrincipalComponent,
+    children:[
+      {path:'dashboard', component:DashboardComponent}
+    ]
+  },
+  {path:'login',component:LoginComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SpeedDialModule } from 'primeng/speeddial';
 import { PrincipalComponent } from './principal/principal.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Error404Component } from './views/error404/error404.component';
+import { MegaMenuModule } from 'primeng/megamenu';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { Error404Component } from './views/error404/error404.component';
     InputTextModule,
     ButtonModule,
     SplitButtonModule,
-    SpeedDialModule
+    SpeedDialModule,
+    MegaMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]

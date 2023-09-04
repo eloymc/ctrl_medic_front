5694447eb46f04aac2641a75accc53c26dfc2510
unknown <Eloy>
2023-09-03 19:40:36 -0600

    se guarda login y nuevos archivos para empezar a iniciar principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './views/login/login.component';
+import { CheckboxModule } from 'primeng/checkbox';
+import { StyleClassModule } from 'primeng/styleclass';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { PrincipalComponent } from './principal/principal.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Error404Component } from './views/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent,
+    DashboardComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CheckboxModule,
+    StyleClassModule,
+    InputTextModule,
+    ButtonModule,
+    SplitButtonModule,
+    SpeedDialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
